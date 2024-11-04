5b34621b050d7ca17ae250e6c54dbff383c9bbbc
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2024-11-04 22:01:34 +0100

    implement auth with firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 const clashGrotesk = localFont({
   src: [
     {
@@ -61,7 +50,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${clashGrotesk.className} antialiased`}
+        className={`${clashGrotesk.className}`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client"
 import Image from "next/image";
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "@/app/firebase/config";
 
 export default function Home() {
+  const [user] = useAuthState(auth);
+  console.log({ user });
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -12,6 +18,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <p>{user?.email}</p>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
